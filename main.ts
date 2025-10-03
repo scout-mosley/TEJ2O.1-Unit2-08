7985daaca0046f1f1cf6cc0f6ebdcf462569adf9
Scout Mosley <scout.mosley@stu.ocsb.ca>
2025-10-03 13:30:31 -0400

    all colors at a ton presdsed'

--- main.ts
+++ main.ts
@@ -5,80 +5,54 @@
  * This program Program now shows all the possible colours on the RGB LED
 */
 
+
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
-
-//when button pressed 
 input.onButtonPressed(Button.A, function () {
-   
-
-//turn on red RGB LED        
-    pins.digitalWritePin(DigitalPin.P13, 1)
-    pins.digitalWritePin(DigitalPin.P15, 0)
-    pins.digitalWritePin(DigitalPin.P16, 0)
+    // turns on red pin
+    basic.showIcon(IconNames.Yes)
+    pins.digitalWritePin(DigitalPin.P14, 1)
     basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P14, 0)
 
-//turn off red RGB LED        
-    pins.digitalWritePin(DigitalPin.P13, 0)
-
-//turn on green RGB LED        
-    pins.digitalWritePin(DigitalPin.P13, 0)
+    // turns on green pin
     pins.digitalWritePin(DigitalPin.P15, 1)
-    pins.digitalWritePin(DigitalPin.P16, 0)
     basic.pause(1000)
-
- //turn off green RGB LED        
     pins.digitalWritePin(DigitalPin.P15, 0)
 
-//turn on blue RGB LED        
-    pins.digitalWritePin(DigitalPin.P13, 0)
-    pins.digitalWritePin(DigitalPin.P15, 0)
+    // turns on blue pin
     pins.digitalWritePin(DigitalPin.P16, 1)
     basic.pause(1000)
-
-//turn off blue RGB LED        
     pins.digitalWritePin(DigitalPin.P16, 0)
 
-//turn on red and green RGB LED        
-    pins.digitalWritePin(DigitalPin.P13, 1)
-    pins.digitalWritePin(DigitalPin.P15, 1)
-    pins.digitalWritePin(DigitalPin.P16, 0)
-    basic.pause(1000)
-
-//turn off red and green RGB LED        
-    pins.digitalWritePin(DigitalPin.P13, 0)
-    pins.digitalWritePin(DigitalPin.P15, 0)
-
-//turn on red and blue RGB LED        
-    pins.digitalWritePin(DigitalPin.P13, 1)
-    pins.digitalWritePin(DigitalPin.P15, 0)
+    // turns on blue and red pin
+    pins.digitalWritePin(DigitalPin.P14, 1)
     pins.digitalWritePin(DigitalPin.P16, 1)
     basic.pause(1000)
-
-//turn off red and blue RGB LED        
-    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P14, 0)
     pins.digitalWritePin(DigitalPin.P16, 0)
 
-//turn on blue and green RGB LED        
-    pins.digitalWritePin(DigitalPin.P13, 0)
+    // turns on green and red pin
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+
+    // turns on green and blue pin
     pins.digitalWritePin(DigitalPin.P15, 1)
     pins.digitalWritePin(DigitalPin.P16, 1)
     basic.pause(1000)
-
-//turn off blue and green RGB LED        
     pins.digitalWritePin(DigitalPin.P15, 0)
     pins.digitalWritePin(DigitalPin.P16, 0)
-    
-//turn on red, blue and green RGB LED        
-    pins.digitalWritePin(DigitalPin.P13, 1)
+
+    // turns on green, red and blue pin
+    pins.digitalWritePin(DigitalPin.P14, 1)
     pins.digitalWritePin(DigitalPin.P15, 1)
     pins.digitalWritePin(DigitalPin.P16, 1)
-    basic.pause(1000)    
-    
-//turn off red, blue and green RGB LED        
-    pins.digitalWritePin(DigitalPin.P13, 0)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P14, 0)
     pins.digitalWritePin(DigitalPin.P15, 0)
     pins.digitalWritePin(DigitalPin.P16, 0)
-
-    })
+})
