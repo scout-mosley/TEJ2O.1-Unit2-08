c224a6feed3f0e23287f4c92e74c8f227da75df3
Scout Mosley <scout.mosley@stu.ocsb.ca>
2025-10-03 13:15:40 -0400

    changes color in a button

--- main.ts
+++ main.ts
@@ -8,82 +8,77 @@
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
+
 //when button pressed 
 input.onButtonPressed(Button.A, function () {
+   
 
 //turn on red RGB LED        
-    basic.clearScreen()
     pins.digitalWritePin(DigitalPin.P13, 1)
-    pins.digitalWritePin(DigitalPin.P14, 0)
     pins.digitalWritePin(DigitalPin.P15, 0)
-    control.waitMicros(1000)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+    basic.pause(1000)
 
 //turn off red RGB LED        
     pins.digitalWritePin(DigitalPin.P13, 0)
 
 //turn on green RGB LED        
-    basic.clearScreen()
     pins.digitalWritePin(DigitalPin.P13, 0)
-    pins.digitalWritePin(DigitalPin.P14, 1)
-    pins.digitalWritePin(DigitalPin.P15, 0)
-    control.waitMicros(1000)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+    basic.pause(1000)
 
-//turn off green RGB LED        
-    pins.digitalWritePin(DigitalPin.P14, 0)
+ //turn off green RGB LED        
+    pins.digitalWritePin(DigitalPin.P15, 0)
 
 //turn on blue RGB LED        
-    basic.clearScreen()
     pins.digitalWritePin(DigitalPin.P13, 0)
-    pins.digitalWritePin(DigitalPin.P14, 0)
-    pins.digitalWritePin(DigitalPin.P15, 1)
-    control.waitMicros(1000)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    basic.pause(1000)
 
 //turn off blue RGB LED        
-    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P16, 0)
 
 //turn on red and green RGB LED        
-    basic.clearScreen()
     pins.digitalWritePin(DigitalPin.P13, 1)
-    pins.digitalWritePin(DigitalPin.P14, 1)
-    pins.digitalWritePin(DigitalPin.P15, 0)
-    control.waitMicros(1000)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+    basic.pause(1000)
 
 //turn off red and green RGB LED        
     pins.digitalWritePin(DigitalPin.P13, 0)
-    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
 
 //turn on red and blue RGB LED        
-    basic.clearScreen()
     pins.digitalWritePin(DigitalPin.P13, 1)
-    pins.digitalWritePin(DigitalPin.P14, 0)
-    pins.digitalWritePin(DigitalPin.P15, 1)
-    control.waitMicros(1000)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    basic.pause(1000)
 
 //turn off red and blue RGB LED        
     pins.digitalWritePin(DigitalPin.P13, 0)
-    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P16, 0)
 
 //turn on blue and green RGB LED        
-    basic.clearScreen()
     pins.digitalWritePin(DigitalPin.P13, 0)
-    pins.digitalWritePin(DigitalPin.P14, 1)
     pins.digitalWritePin(DigitalPin.P15, 1)
-    control.waitMicros(1000)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    basic.pause(1000)
 
 //turn off blue and green RGB LED        
-    pins.digitalWritePin(DigitalPin.P14, 0)
     pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P16, 0)
     
 //turn on red, blue and green RGB LED        
-    basic.clearScreen()
     pins.digitalWritePin(DigitalPin.P13, 1)
-    pins.digitalWritePin(DigitalPin.P14, 1)
     pins.digitalWritePin(DigitalPin.P15, 1)
-    control.waitMicros(1000)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    basic.pause(1000)    
     
 //turn off red, blue and green RGB LED        
     pins.digitalWritePin(DigitalPin.P13, 0)
-    pins.digitalWritePin(DigitalPin.P14, 0)
     pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P16, 0)
 
     })
